2d43d10206bd438be967bfe958af2320631e97b4
Jainish006 <jainishnshah@gmail.com>
2026-03-07 10:31:12 +0530

    Chatbot updated

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { DashboardSidebar } from '@/components/dashboard/sidebar'
 import { DashboardHeader } from '@/components/dashboard/header'
+import { ChatbotLauncher } from '@/components/dashboard/chatbot-launcher'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
@@ -27,16 +28,18 @@ export default async function DashboardLayout({
     .maybeSingle()
 
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen w-full bg-background">
-        <DashboardSidebar user={user} profile={profile} />
-        <div className="flex flex-1 flex-col">
-          <DashboardHeader user={user} />
-          <main className="flex-1 p-6 overflow-auto">
-            {children}
-          </main>
+    <ChatbotLauncher>
+      <SidebarProvider>
+        <div className="flex min-h-screen w-full bg-background">
+          <DashboardSidebar user={user} profile={profile} />
+          <div className="flex flex-1 flex-col">
+            <DashboardHeader user={user} />
+            <main className="flex-1 p-6 overflow-auto">
+              {children}
+            </main>
+          </div>
         </div>
-      </div>
-    </SidebarProvider>
+      </SidebarProvider>
+    </ChatbotLauncher>
   )
 }
